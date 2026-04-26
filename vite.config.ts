7e8950e36a43707ff9ef46570f9ad1711cff93a5
Ravi Padala <Ravi@bcatalysts.com>
2026-04-26 12:30:54 +0530

    PWA changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,96 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      base: '/time-tracker/',
+      includeAssets: [
+        'icons/icon-48x48.png',
+        'icons/icon-72x72.png',
+        'icons/icon-96x96.png',
+        'icons/icon-128x128.png',
+        'icons/icon-144x144.png',
+        'icons/icon-192x192.png',
+        'icons/icon-256x256.png',
+        'icons/icon-384x384.png',
+        'icons/icon-512x512.png',
+        'maskable-icon.png',
+        'apple-touch-icon.png',
+        'favicon.ico',
+      ],
+      manifest: {
+        short_name: "Time Track",
+        name: "Time Tracker PWA",
+        icons: [
+          {
+            src: 'icons/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#1976d2",
+        background_color: "#fafafa"
+      }
+    })
+  ],
+  base: "/time-tracker/",
+});
